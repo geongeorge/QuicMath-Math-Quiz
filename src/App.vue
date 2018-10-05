3ea4a22d466ffb40b7ee1fecf0f8a2087b81ed2d
unknown <geongeorge98@gmail.com>
2018-10-05 22:21:57 +0530

    Initial design of Question component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Question></Question>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Question from './components/Question.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Question
   }
 }
 </script>
---
+++ src/components/Question.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container hello">
+    <p class="question">What is <span class="bld">4 + 2</span>?</p>
+    <div class="columns answers">
+      <div class="column">
+        <button class="button is-light is-large is-fullwidth">3</button>
+      </div>
+      <div class="column">
+        <button class="button is-light is-large is-fullwidth">3</button>
+      </div>
+    </div>
+    <div class="columns answers">
+      <div class="column">
+        <button class="button is-light is-large is-fullwidth">3</button>
+      </div>
+      <div class="column">
+        <button class="button is-light is-large is-fullwidth">3</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  color: #636363;
+  font-size: 40px;
+  max-width: 400px;
+  margin: 0 auto;
+}
+.hello .bld{
+  color: #323232;
+  font-weight: bold;
+}
+.answers {
+  margin: 10px auto;
+  font-weight: bold;
+  color: #636363;
+}
+.answers:hover {
+  color: #323232;
+}
+.answers .columnn{
+  padding-left: 10px;
+  padding-right: 10px;
+}
+</style>
